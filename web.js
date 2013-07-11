2ab265b76956e233c5050f34d82787cabc9940ac
William Sehorn <william.sehorn@gmail.com>
2013-07-11 23:53:51 +0000

    Now reads text from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
+var fs = require('fs');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readFileSync('index.html', {ecoding:'utf-8'}));
 });
 
 var port = process.env.PORT || 5000;
